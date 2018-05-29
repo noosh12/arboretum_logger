755379bd24cb33dafd03412fa93ceb0bdfdca082
Unknown <dsandirigama@gmail.com>
2018-05-29 20:35:53 +1000

    added regex sub on node identifier
    ensures that conditional statements will always work

--- logger.py
+++ logger.py
@@ -6,6 +6,7 @@ import xbee
 #import settings
 import os
 import csv
+import re
 
 
 def main():
@@ -30,15 +31,22 @@ def log():
         samples = data_samples['samples'][0]
 
         #node_id = str(data_samples['source_addr_long'])
-        node_id = str(data_samples['source_addr'])
+        node_id = str(data_samples['source_addr_long'])
+        node_id = re.sub('[^A-Za-z0-9]+', '', node_id)
         time = datetime.now()
 
         vwc_600 = ((11.9*(((float(samples['adc-0'])*1200)/1023)/10000))-0.401)*100
         vwc_300 = ((11.9*(((float(samples['adc-1'])*1200)/1023)/10000))-0.401)*100
         temp_100 = ((((float(samples['adc-2'])*1.2)/1023)*3)*41.67)-40
         battery = float(samples['adc-3'])
+
+        node1 = "bx00x13xa2x00Abx9cZ"
+        node2 = "bx00x13xa2x00Abx9cxbf"
         
         log_write(node_id, battery)
+
+
+
 
 
 def log_write(node_id, battery):
