2d79ececf8c173d52e1163d712b4879cdab2b5e1
Unknown <dsandirigama@gmail.com>
2018-05-29 21:13:52 +1000

    added sample averaging

--- logger.py
+++ logger.py
@@ -26,6 +26,12 @@ def log():
     
     print("initialising serial port listening...")
 
+    sample_count = 0
+    node1_battery = 0.0
+    node2_battery = 0.0
+    node1 = "bx00x13xa2x00Abx9cZ"
+    node2 = "bx00x13xa2x00Abx9cxbf"
+
     while True:
         data_samples = xbee1.wait_read_frame()
         samples = data_samples['samples'][0]
@@ -39,11 +45,42 @@ def log():
         vwc_300 = ((11.9*(((float(samples['adc-1'])*1200)/1023)/10000))-0.401)*100
         temp_100 = ((((float(samples['adc-2'])*1.2)/1023)*3)*41.67)-40
         battery = float(samples['adc-3'])
-
-        node1 = "bx00x13xa2x00Abx9cZ"
-        node2 = "bx00x13xa2x00Abx9cxbf"
         
-        log_write(node_id, battery)
+        if(sample_count < 2):
+            if(node_id == node1):
+                node1_battery = battery
+                print('n1 first: '+str(battery))
+            elif(node_id == node2):
+                node2_battery = battery
+                print('n2 first: '+str(battery))
+            sample_count+=1
+
+        elif(sample_count == 2):
+            if(node_id == node1):
+                print('n1 second: '+str(battery))
+                node1_battery += battery
+                node1_battery = node1_battery/2.0
+            elif(node_id == node2):
+                print('n2 second: '+str(battery))
+                node2_battery += battery
+                node2_battery = node2_battery/2.0
+            sample_count+=1
+        else:
+            if(node_id == node1):
+                print('n1 second: '+str(battery))
+                node1_battery += battery
+                node1_battery = node1_battery/2.0
+                log_write('node1', node1_battery)
+                log_write('node2', node2_battery)
+            elif(node_id == node2):
+                print('n2 second: '+str(battery))
+                node2_battery += battery
+                node2_battery = node2_battery/2.0
+                log_write('node1', node1_battery)
+                log_write('node2', node2_battery)
+            sample_count = 0
+
+
 
 
 
